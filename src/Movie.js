668c3060ed7c640b925874b137e4600d253cca53
Admin <admin@Mac-Admin.local>
2017-11-06 15:09:25 +0300

    jquery added, get query example in movies/{movieId}

--- src/Movie.js
+++ src/Movie.js
@@ -6,21 +6,42 @@ import TapMovieCollection from './TapMovieCollection'
 import TapSidebarCollection from './TapSidebarCollection'
 import { Segment, Grid, Sticky, Item } from 'semantic-ui-react'
 import _ from 'lodash'
+import $ from 'jquery'; 
 
 import { Button, Icon } from 'semantic-ui-react'
 
 class Movies extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {movie: []};
+  }
+
+  componentDidMount()
+  {
+    $.getJSON('https://randomuser.me/api/')
+    .then(({ results }) => this.setState({ movie: results }));
+  }
+
     state = {}
 
     handleContextRef = contextRef => this.setState({ contextRef })
 
   render() {
-      const { contextRef } = this.state
+    const movies = this.state.movie.map((item, i) => (
+      <div>
+        <h1>{ item.name.first }</h1>
+        <span>{ item.cell }, { item.email }</span>
+      </div>
+    ));
+
     return (
       <div >
             <h3>
                 Movie {this.props.match.params.movieId.toString()}
             </h3>
+            <div className="panel-list">{ movies }</div>
       </div>
     );
   }
