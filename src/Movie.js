1daa1cd68de7e619c8fa3af2b5d5103b82026e8a
hovhannest <hovhannes.ht@gmail.com>
2017-11-07 21:19:05 +0400

    merge from nvard

--- src/Movie.js
+++ src/Movie.js
@@ -4,15 +4,13 @@ import TapMenu from './TapMenu'
 import TapFooter from './TapFooter'
 import TapMovieCollection from './TapMovieCollection'
 import TapSidebarCollection from './TapSidebarCollection'
-import { Segment, Grid, Sticky, Item, Image } from 'semantic-ui-react'
+import { Segment, Grid, Sticky, Item } from 'semantic-ui-react'
 import _ from 'lodash'
 import $ from 'jquery'; 
 import {
   Route
 } from 'react-router-dom';
 import NotFound from './NotFound'
-
-import { Button, Icon } from 'semantic-ui-react'
 
 class Movie extends Component {
 
@@ -58,13 +56,41 @@ class Movie extends Component {
    
 
     return (
-      <div >
-            <h3>
-                Movie {this.props.match.params.movieId.toString()}
-            </h3> 
+        <Item.Group>
+          <Item >
+            <Item.Image src={this.state.movie.URL} />
 
-             <Image src={this.state.movie.URL} size='medium' rounded /> 
-      </div>
+            <Item.Content>
+              <Item.Header>
+                Movie {this.props.match.params.movieId.toString()}
+              </Item.Header>
+              <Item.Description>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, consectetur cum delectus dolor dolorem
+                dolorum earum eius ex impedit labore magni mollitia neque nihil omnis, quas quidem, reiciendis saepe voluptates?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor error illum minus necessitatibus nemo
+                nisi perferendis quibusdam rem temporibus. Aliquam ducimus eius hic nihil quas. A adipisci dolorem quis soluta.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. At aut commodi delectus dolore dolorem
+                xdoloremque dolores ducimus error explicabo fuga ipsam nemo, odio praesentium quo quod rerum voluptatibus! Quidem, voluptas.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, consectetur cum delectus dolor dolorem
+                dolorum earum eius ex impedit labore magni mollitia neque nihil omnis, quas quidem, reiciendis saepe voluptates?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor error illum minus necessitatibus nemo
+                nisi perferendis quibusdam rem temporibus. Aliquam ducimus eius hic nihil quas. A adipisci dolorem quis soluta.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. At aut commodi delectus dolore dolorem
+                xdoloremque dolores ducimus error explicabo fuga ipsam nemo, odio praesentium quo quod rerum voluptatibus! Quidem, voluptas.Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, consectetur cum delectus dolor dolorem
+                dolorum earum eius ex impedit labore magni mollitia neque nihil omnis, quas quidem, reiciendis saepe voluptates?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor error illum minus necessitatibus nemo
+                nisi perferendis quibusdam rem temporibus. Aliquam ducimus eius hic nihil quas. A adipisci dolorem quis soluta.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. At aut commodi delectus dolore dolorem
+                xdoloremque dolores ducimus error explicabo fuga ipsam nemo, odio praesentium quo quod rerum voluptatibus! Quidem, voluptas.Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, consectetur cum delectus dolor dolorem
+                dolorum earum eius ex impedit labore magni mollitia neque nihil omnis, quas quidem, reiciendis saepe voluptates?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor error illum minus necessitatibus nemo
+                nisi perferendis quibusdam rem temporibus. Aliquam ducimus eius hic nihil quas. A adipisci dolorem quis soluta.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. At aut commodi delectus dolore dolorem
+                xdoloremque dolores ducimus error explicabo fuga ipsam nemo, odio praesentium quo quod rerum voluptatibus! Quidem, voluptas.
+              </Item.Description>
+            </Item.Content>
+          </Item>
+        </Item.Group>
     );
   }
 }
