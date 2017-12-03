d2419fb8104fa64e372d98bf2688a14c3257cbe1
a-vrtn <vartanian.anooshik@gmail.com>
2017-12-03 15:19:50 +0400

    login semi functional

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Button, Segment, Form, View } from 'semantic-ui-react';
 import './Login.css';
-import Password from 'try-react-password-input'
+// import Password from 'try-react-password-input'
 
 
 
@@ -24,57 +24,6 @@ class Login extends Component{
 			alert("Please enter the password");
 		}
 
-		
-fetch('http://localhost:3000/login', {
-  method: 'POST',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-  },
-
-  body: JSON.stringify({
-    login: username,
-   password: password,
-  })
-}).then(function(response){  if( response.status=='200' && response.ok == true) {
-	return response.json();
-} }).then( function(data){})
-
-*/
-
-/* or */
-/*var form = new FormData(document.getElementById('login-form'));
-fetch("/login", {
-  method: "POST",
-  body: form
-});*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		/*var request = new XMLHTTPRequest();
-		request.open('POST','URL', true,{username,password});
-		request.send();
-		request.addEventListener("readystatechange",requestHandler, false);*/
-		/*if(request.readyState==4   && request.status==200) *//*  EVERYTHING IS COMPLETED AND SUCCESSFULLY*/
-			/* HERE WE MUST REDIRECT USER TO ITS PROFILE PAGE*/
-		/*{
-			var response= JSON.parse(request.responseText);
-		}*/
-
 
 
 	} 
@@ -92,7 +41,7 @@ fetch("/login", {
 					<div className="formInput">
 						<Form.Input id ="userName"   placeholder='User name' />
 						<div style={{}}>
-						<Password   ref="password" placeholder='Password' uppercase lowercase digits  className="password-input" />
+						<input   placeholder='Password' type="password" className="password-input" />
   
   
   
